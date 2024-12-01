7294e224d918b06fd2528080aa6deb5b8a632362
Olanti <olanti-p@yandex.ru>
2024-12-01 10:20:47 +0300

    New music tracks

---
+++ scenes/globals/music_controller.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func play_main_menu() -> void:
+	pass
+
+func play_map() -> void:
+	pass
+
+func play_clouds() -> void:
+	pass
+
+func play_bastion() -> void:
+	pass
+
+func play_ice() -> void:
+	pass
+
+func play_glitch() -> void:
+	pass
