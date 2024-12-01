a43349a5fefcf507896b07d8255a3b646f69ee3b
Olanti <olanti-p@yandex.ru>
2024-12-02 00:33:39 +0300

    last changes

--- scenes/globals/all_runs.gd
+++ scenes/globals/all_runs.gd
@@ -4,35 +4,35 @@ var total_time_in_game: float = 0.0
 
 var current_level: Level = null
 
-#var cheat_reset: bool = false
-#var cheat_reveal: bool = false
-#var cheat_infinite_dash: bool = false
-#var cheat_double_jump: bool = false
-#var cheat_no_fall_damage: bool = false
-#var cheat_unlock_map: bool = false
-#var cheat_no_spike_damage: bool = false
-#var cheat_infinite_stamina: bool = false
-#var cheat_no_star_damage: bool = false
-#var cheat_reverse_gravity: bool = false
-#var cheat_no_laser_damage: bool = false
-#var cheat_unstuck: bool = false
-#var cheat_noclip: bool = false
-#var cheat_timer: bool = false
-
-var cheat_reset: bool = true
-var cheat_reveal: bool = true
-var cheat_infinite_dash: bool = true
-var cheat_double_jump: bool = true
-var cheat_no_fall_damage: bool = true
-var cheat_unlock_map: bool = true
-var cheat_no_spike_damage: bool = true
-var cheat_infinite_stamina: bool = true
-var cheat_no_star_damage: bool = true
-var cheat_reverse_gravity: bool = true
-var cheat_no_laser_damage: bool = true
-var cheat_unstuck: bool = true
-var cheat_noclip: bool = true
-var cheat_timer: bool = true
+var cheat_reset: bool = false
+var cheat_reveal: bool = false
+var cheat_infinite_dash: bool = false
+var cheat_double_jump: bool = false
+var cheat_no_fall_damage: bool = false
+var cheat_unlock_map: bool = false
+var cheat_no_spike_damage: bool = false
+var cheat_infinite_stamina: bool = false
+var cheat_no_star_damage: bool = false
+var cheat_reverse_gravity: bool = false
+var cheat_no_laser_damage: bool = false
+var cheat_unstuck: bool = false
+var cheat_noclip: bool = false
+var cheat_timer: bool = false
+#
+#var cheat_reset: bool = true
+#var cheat_reveal: bool = true
+#var cheat_infinite_dash: bool = true
+#var cheat_double_jump: bool = true
+#var cheat_no_fall_damage: bool = true
+#var cheat_unlock_map: bool = true
+#var cheat_no_spike_damage: bool = true
+#var cheat_infinite_stamina: bool = true
+#var cheat_no_star_damage: bool = true
+#var cheat_reverse_gravity: bool = true
+#var cheat_no_laser_damage: bool = true
+#var cheat_unstuck: bool = true
+#var cheat_noclip: bool = true
+#var cheat_timer: bool = true
 
 func _process(delta: float) -> void:
 	total_time_in_game += delta
