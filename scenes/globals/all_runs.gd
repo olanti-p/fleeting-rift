53e5957286619e8fc728dcdddf4a0ae4472fe7ac
Olanti <olanti-p@yandex.ru>
2025-01-09 20:06:19 +0300

    Add debug keys to enable/disable all cheat codes

--- scenes/globals/all_runs.gd
+++ scenes/globals/all_runs.gd
@@ -53,40 +53,40 @@ var cheat_no_laser_damage: bool = false
 var cheat_unstuck: bool = false
 var cheat_noclip: bool = false
 var cheat_timer: bool = false
-#
-#var cheat_reset: bool = true
-#var cheat_reveal: bool = true
-#var cheat_infinite_dash: bool = true
-#var cheat_double_jump: bool = true
-#var cheat_no_fall_damage: bool = true
-#var cheat_unlock_map: bool = true
-#var cheat_no_spike_damage: bool = true
-#var cheat_infinite_stamina: bool = true
-#var cheat_no_star_damage: bool = true
-#var cheat_reverse_gravity: bool = true
-#var cheat_no_laser_damage: bool = true
-#var cheat_unstuck: bool = true
-#var cheat_noclip: bool = true
-#var cheat_timer: bool = true
+
+var all_cheats = [
+	"cheat_reset",
+	"cheat_reveal",
+	"cheat_infinite_dash",
+	"cheat_double_jump",
+	"cheat_no_fall_damage",
+	"cheat_unlock_map",
+	"cheat_no_spike_damage",
+	"cheat_infinite_stamina",
+	"cheat_no_star_damage",
+	"cheat_reverse_gravity",
+	"cheat_no_laser_damage",
+	"cheat_unstuck",
+	"cheat_noclip",
+	"cheat_timer",
+]
+
+
+func _set_all_cheats(value: bool) -> void:
+	for cheat in all_cheats:
+		set(cheat, value)
+
 
 func _process(delta: float) -> void:
 	total_time_in_game += delta * get_difficulty_time_correction()
+	if Input.is_action_just_pressed("debug_enable_all_cheats"):
+		_set_all_cheats(true)
+	if Input.is_action_just_pressed("debug_disable_all_cheats"):
+		_set_all_cheats(false)
+
 
 func factory_reset() -> void:
 	total_time_in_game = 0.0
-	cheat_reset = false
-	cheat_reveal = false
-	cheat_infinite_dash = false
-	cheat_double_jump = false
-	cheat_no_fall_damage = false
-	cheat_unlock_map = false
-	cheat_no_spike_damage = false
-	cheat_infinite_stamina = false
-	cheat_no_star_damage = false
-	cheat_reverse_gravity = false
-	cheat_no_laser_damage = false
-	cheat_unstuck = false
-	cheat_noclip = false
-	cheat_timer = false
+	_set_all_cheats(false)
 	SceneTransition.play_mm = true
 	SceneTransition.do_glitched("res://scenes/levels/level_main_menu.tscn")
