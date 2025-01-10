4aa64484c41010a2f680a573ece0b392d354c7ed
Olanti <olanti-p@yandex.ru>
2025-01-10 22:50:39 +0300

    Fix camera jitter in level_win

--- scenes/levels/level_win.gd
+++ scenes/levels/level_win.gd
@@ -6,8 +6,12 @@ func _ready() -> void:
 	is_glitch_level = true
 	MusicController.play_finale()
 
+
 func _process(delta: float) -> void:
 	super(delta)
-	$MainCamera.limit_top = 0
-	$MainCamera.limit_bottom = 240
-	$MainCamera.global_position = $Player.global_position
+
+
+func _physics_process(_delta: float) -> void:
+	main_camera.limit_top = 0
+	main_camera.limit_bottom = 240
+	main_camera.global_position = $Player.global_position
